3bf1ef0fc481f4cbc8cf4fdf94af258031b3489a
Belal <belal_ph13@yahoo.com>
2021-08-10 23:22:33 +0200

    added Product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,22 @@
+class Product {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String image;
+  Product(
+      {required this.id,
+      required this.title,
+      required this.price,
+      required this.description,
+      required this.image});
+
+  factory Product.fromJson(Map<String, dynamic> parsedJson) {
+    return Product(
+        id: parsedJson['id'],
+        title: parsedJson['title'],
+        price: parsedJson['price'],
+        description: parsedJson['description'],
+        image: parsedJson['description']);
+  }
+}
